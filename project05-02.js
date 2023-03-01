5b090c9df9ac2555db7bc3c15ece68c327cae45c
Javirb26 <112392832+Javirb26@users.noreply.github.com>
2023-03-01 07:58:32 -0500

    Add files via upload
    JavaScript Web for Warriors: Chapter 5 Hands on Project 2

---
+++ project05-02.js
@@ -0,0 +1,32 @@
+"use strict";
+/*    JavaScript 7th Edition
+      Chapter 5
+      Project 05-02
+
+      Project to move images between a photo bucket and photo list.
+      Author: Javier Ballester
+      Date: 02/28/2023   
+
+      Filename: project05-02.js
+*/
+
+let images = document.getElementsByTagName("img");
+let photoBucket = document.getElementById("photo_bucket");
+let photoList = document.getElementById("photo_list");
+
+
+for (let i=0; i < images.length; i++) {
+   images[i].onclick = function() {
+      if (this.parentElement.id === "photo_bucket") { 
+         let newItem = document.createElement("li");
+         photoList.appendChild(newItem);
+         newItem.appendChild(this);
+      } else {
+         let oldItem = this.parentElement;
+         photoBucket.appendChild(this);
+         oldItem.parentElement.removeChild(oldItem);
+      }
+   }
+}
+
+
